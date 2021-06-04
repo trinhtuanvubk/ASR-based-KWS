21e3719ae39ee6a287e6b6119e03834d3675e1f7
Tuan Vu Trinh <vutuantrinh2000@gmail.com>
2021-06-04 20:58:44 +0700

    Update run_demo.py

--- run_demo.py
+++ run_demo.py
@@ -1,6 +1,3 @@
 import scenario 
-# import util
-# from util import stream \
-# model_path = "./lightning_logs/version_7/checkpoints/epoch=2-step=21404.ckpt"
-# asr_model = nemo_asr.models.EncDecCTCModel.load_from_checkpoint(checkpoint_path = model_path)
+# run demo 
 scenario.demo()
